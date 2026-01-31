00570b4cc27160f0d69fe304e19e5140c12062f4
Milo van Pelt <milovanpelt@gmail.com>
2026-01-31 12:29:12 +0100

    refactor: shorter code for scale spawning

--- PuzzleBox/Assets/Scripts/PuzzleManager.cs
+++ PuzzleBox/Assets/Scripts/PuzzleManager.cs
@@ -38,8 +38,8 @@ public class PuzzleManager : MonoBehaviour
             {
                 var newPuzzle = Instantiate(PuzzlesToSpawn[i], position, puzzleTransform.rotation);
                 newPuzzle.transform.SetParent(puzzleTransform);
-                Vector3 scale = new Vector3(1f,1f,1f);
-                newPuzzle.transform.localScale = scale;
+                newPuzzle.transform.localScale = Vector3.one;
+
                 activePuzzles.Add(newPuzzle);
             }
         }
