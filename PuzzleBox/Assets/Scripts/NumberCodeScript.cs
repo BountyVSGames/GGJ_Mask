9743cae59f707464053121cb5b75839a79b1a5cd
Szymon Tydrych <szymon.tydrych@gmail.com>
2026-01-31 15:46:09 +0100

    [ADD] added number lock logic

---
+++ PuzzleBox/Assets/Scripts/NumberCodeScript.cs
@@ -0,0 +1,79 @@
+using Mono.Cecil.Cil;
+using System.Diagnostics;
+using System.Linq;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class NumberCodeScript : MonoBehaviour
+{
+
+    private Camera m_Camera;
+    public string Number;
+    string curentNumber;
+    public GameObject[] buttons;
+    public TextMeshPro codeDisplay;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        m_Camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RayCast();
+    }
+
+    private void RayCast()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+
+            if (Input.GetMouseButtonDown(0))
+        {
+            print("Cliced");
+            Ray ray = m_Camera.ScreenPointToRay(mousePosition);
+            RaycastHit hit;
+            
+            if (Physics.Raycast(ray, out hit))   
+            {
+                print("hit"); 
+                foreach (GameObject button in buttons)
+                {
+                   // UnityEngine.Debug.Log(hit.collider.gameObject.name, hit.collider.gameObject);
+
+                    if (hit.collider.gameObject == button)
+                    {
+                        Transform child = button.transform.GetChild(0);
+                        TextMeshPro tmp = child.GetComponent<TextMeshPro>();
+                       
+                        curentNumber += tmp.text;
+                        print(curentNumber);
+                        codeDisplay.text = curentNumber;
+                        CheckNummber();
+                        
+                    }
+                }
+            }
+
+        }
+    }
+
+    void CheckNummber()
+    {
+        if (curentNumber.Length >= 4)
+        {
+            if (curentNumber == Number)
+            {
+                print("open");
+                Object.Destroy(this.gameObject); 
+            }
+            else
+            {
+                curentNumber = "";
+            }
+        }
+    }
+}
